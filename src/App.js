7514d4c068b27c70fda2843c13e7e2cdaaf11395
Anton <ucaboro@ucl.ac.uk>
2018-08-16 23:12:22 +0300

    Added Mobile/Desktop conditioning, modified book component to adjust size, created reusable containers for "Found books" / "Reading"

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import './App.css';
 import {Columns, Column, Button, Title, Subtitle, Field, Control, Input, Box} from 'bloomer';
 import Book from './Components/book.js'
 
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
+
 class App extends Component {
 
-getAllBooks = (num) => {
+getAllBooks = (num, size) => {
   let books = []
   for (let i=1; i<num; i++){
-    books.push( <Book key ={i} title={"title " + i} subtitle={"subtitle " + i}/> )
+    books.push( <Book key ={i} title={"title " + i} subtitle={"subtitle " + i} size={size}/> )
    }
 
    return books
 }
 
   render() {
-
     return(
       <div className="App">
         <Columns isCentered isMultiline  >
@@ -24,45 +30,27 @@ getAllBooks = (num) => {
             <Title isSize={2}>Read X in Y</Title>
           </Column>
 
+
           <Column isSize={12}>
               <Field isHorizontal hasAddons="centered">
                 <Control className="searchBooksInput">
-                  <Input isSize="medium"   type="text" placeholder="Start typing"/>
+                  <Input isSize="medium" type="text" placeholder="Start typing"/>
                   <span className="underline"></span>
                 </Control>
                </Field>
           </Column>
 
+
+
           <Column>
-            <Columns isCentered>
-              <Column>
-                <Box>
-                  <Subtitle isSize={3}>READING</Subtitle>
-                  <div className="divider"/>
-                  <div className="placeholder"/>
-                </Box>
-              </Column>
+            <Columns isCentered >
+            <ReadingContainer title="READING"/>
+            {isBrowser
+              ?  <FoundContainerDesktop books={this.getAllBooks(9, 3)}/>
+              : <FoundContainerMobile books={this.getAllBooks(9, 4)}/> }
 
-              <Column isSize={5}>
-                <Box className="foundBox">
-                  <Subtitle  isSize={3}>FOUND</Subtitle>
-                  <div className="divider"/>
 
-                  <Columns isMobile className="foundScrollableContainer scrollBar">
-                  {this.getAllBooks(8)}
-                  </Columns>
-                </Box>
-                 <Button isColor='info' isSize="large" className="customButton" isOutlined>DONE</Button>
-              </Column>
-
-              <Column>
-                <Box>
-                  <Subtitle  isSize={3}>TO READ</Subtitle>
-                  <div className="divider"/>
-                  <div className="placeholder"/>
-                </Box>
-              </Column>
-
+            <ReadingContainer title="TO READ"/>
             </Columns>
           </Column>
         </Columns>
@@ -71,5 +59,50 @@ getAllBooks = (num) => {
     );
   }
 }
+
+const ReadingContainer = ({title}) => {
+  if (isBrowser){
+    return (
+      <Column>
+        <Box>
+          <Subtitle isSize={3}>{title}</Subtitle>
+          <div className="divider"/>
+          <div className="placeholder"/>
+        </Box>
+      </Column>
+    )
+  }
+    return ("")
+}
+
+const FoundContainerDesktop = (props) => (
+  <Column isSize={5}>
+    <Box className="foundBox">
+      <Subtitle  isSize={3}>FOUND</Subtitle>
+      <div className="divider"/>
+      <Columns isMobile isMultiline className="foundScrollableDesktop scrollBar">
+      {props.books}
+      </Columns>
+    </Box>
+     <Button isColor='info' isSize="large" className="customButton" isOutlined>DONE</Button>
+  </Column>
+)
+
+const FoundContainerMobile = (props) => (
+  <Column isSize={5}>
+    <Box className="foundBox">
+      <Subtitle  isSize={3}>FOUND</Subtitle>
+      <div className="divider"/>
+      <Columns isMobile className="foundScrollableMobile scrollBar">
+      {props.books}
+      </Columns>
+    </Box>
+     <Button isColor='info' isSize="large" className="customButton" isOutlined>DONE</Button>
+  </Column>
+)
+
+
+
+
 
 export default App;
--- src/Components/book.js
+++ src/Components/book.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Column, Title, Subtitle, Image} from 'bloomer';
 
-const Book = ({title, subtitle}) => (
-    <Column isSize={4}>
+const Book = ({title, subtitle, size}) => (
+    <Column isSize={size}>
         <Image isRatio="1:2" src="https://bulma.io/images/placeholders/480x800.png" />
         <Title isSize={5} style={{paddingTop: '15px'}}>{title}</Title>
         <Subtitle isSize={6}>{subtitle}</Subtitle>
